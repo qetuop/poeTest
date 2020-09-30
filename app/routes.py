49a210ea226a21a2a7041c7b87838776e0aae9c7
qetuop <bdonson@yahoo.com>
2020-09-30 12:56:07 -0400

    fixed func calls

--- app/routes.py
+++ app/routes.py
@@ -8,24 +8,20 @@ from app.forms import LoginForm
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
-    leagues = poeq.getLeagues()
+    leagues = poeq.getLeagueNames()
     form.league.choices = [(league, league) for league in leagues]
 
     if form.validate_on_submit():
-        print('here')
         account = form.account.data
         league = form.league.data
         poesessid = form.poessid.data
 
         poeq.setup(league, account, poesessid)
 
-        numTabs = poeq.getNumTabs()  # {'numTabs': 28}  or {'error': {'code': 6, 'message': 'Forbidden'}}
+        numTabs = poeq.getNumTabs(league)  # {'numTabs': 28}  or {'error': {'code': 6, 'message': 'Forbidden'}}
         print(numTabs)
         form.tabs.data = numTabs
 
-
-        #flash('NOU3')
-        #return redirect('login')
         return render_template('index.html', form=form)
 
 
